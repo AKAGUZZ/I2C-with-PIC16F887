bae279809c6b40bdf6067eeaa25929fdb83eefbc
Jose Angel Reyes Guzmán <guzz.01me@gmail.com>
2024-08-03 00:31:32 -0600

    Update i2c.h

--- Slave/i2c.h
+++ Slave/i2c.h
@@ -1,33 +1,18 @@
 /* #include "i2c.h"
 *
 * Creada por: Ing. Abiezer Hernandez O.
+* Modificado y adaptado por: Estudiante Mecatrónico Reyes Guzmán Jose Angel
 * Fecha de creacion: 25/11/2019
 * Electronica y Circuitos
 *
+* Comentario: Archivo que contiene los prototipos de funciones para la gestión y control de la comunicación i2c en un
+* PIC16F887 en modo SLAVE.
 */
 
 #include <xc.h>
 #define _XTAL_FREQ 8000000
 
-#define TRIS_SCL TRISBbits.TRISB1
-#define TRIS_SDA TRISBbits.TRISB0
-
-#define I2C_100KHZ 0x80
-#define I2C_400KHZ 0x00
-
-//#define I2C_MASTER_MODE
 #define I2C_SLAVE_MODE
-
-#ifdef I2C_MASTER_MODE
-void I2C_Init_Master(unsigned char sp_i2c);
-void I2C_Start(void);
-void I2C_Stop(void);
-void I2C_Restart(void);
-void I2C_Ack(void);
-void I2C_Nack(void);
-unsigned char I2C_Read(void);
-short I2C_Write(char data);
-#endif
 
 #ifdef I2C_SLAVE_MODE
 void I2C_Init_Slave(unsigned char add_slave);
